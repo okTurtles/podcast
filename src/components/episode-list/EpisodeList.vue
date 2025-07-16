4cd9ee1b028d80999b43fd47a7749d378c4c4709
SebinSong <songxevin@gmail.com>
2025-07-16 16:55:40 +0900

    add another sample episode .md file / implement fetching sorted episodes

--- src/components/episode-list/EpisodeList.vue
+++ src/components/episode-list/EpisodeList.vue
@@ -1,34 +1,16 @@
 <template>
   <div>
     <ul>
-      <li v-for="episode in episodes" :key="episode.slug">
-        {{ episode.title }}
+      <li v-for="episode in episodes" :key="episode.frontmatter.permalink">
+        EP {{ episode.frontmatter.episode }}. - {{ episode.frontmatter.title }}
       </li>
     </ul>
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
+import { ref } from 'vue'
+import { getAllEpisodes } from '@/helpers'
 
-const episodes = ref([
-  {
-    title: 'Episode 1',
-    slug: 'episode-1',
-    description: 'This is the first episode',
-    date: '2025-01-01',
-  },
-  {
-    title: 'Episode 2',
-    slug: 'episode-2',
-    description: 'This is the second episode',
-    date: '2025-01-02',
-  },
-  {
-    title: 'Episode 3',
-    slug: 'episode-3',
-    description: 'This is the third episode',
-    date: '2025-01-03',
-  },
-])
+const episodes = ref(getAllEpisodes())
 </script>
--- src/helpers.ts
+++ src/helpers.ts
@@ -17,3 +17,16 @@ export function classNames(
 
   return `${defaultClasses} ${filteredClasses}`.trim()
 }
+
+export function getAllEpisodes ({ sortBy = 'latest' } = {}): Array<any> {
+  const allPosts = Object.values(import.meta.glob('./pages/episodes/*.{md,mdx}', { eager: true })) 
+  const sorter = (a: any, b: any) => {
+    const aEpisode = a.frontmatter.episode
+    const bEpisode = b.frontmatter.episode
+    return sortBy === 'latest'
+      ? bEpisode - aEpisode
+      : aEpisode - bEpisode
+  }
+
+  return allPosts.sort(sorter)
+}
