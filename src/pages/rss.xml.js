1144cea9c49372de01af0f2b04dea4682efa9041
SebinSong <songxevin@gmail.com>
2025-07-21 17:29:25 +1200

    improve RSS-feed generation process

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,7 +1,20 @@
 import path from 'node:path'
-import rss, { pagesGlobToRssItems} from '@astrojs/rss'
-import { SITE_TITLE_COMMON, SITE_SUBTITLE_COMMON, SITE_DESCRIPTION_COMMON,
-  SITE_AUTHOR, PODCAST_SUMMARY } from '@/constants'
+import rss from '@astrojs/rss'
+import sanitizeHtml from 'sanitize-html'
+import {
+  SITE_TITLE_COMMON, SITE_SUBTITLE_COMMON, SITE_DESCRIPTION_COMMON,
+  SITE_AUTHOR, PODCAST_SUMMARY
+} from '@/constants'
+
+// NOTE - This RSS feed generator script was written based on the guidelines in the following resources:
+// Required tags in RSS feed of a podcast: https://help.apple.com/itc/podcasts_connect/#/itcb54353390
+// Podcast RSS feed requirements for Apple Podcasts: https://podcasters.apple.com/support/823-podcast-requirements
+
+const sanitizePostHTML = (content) => {
+  return sanitizeHtml(content, {
+    allowedTags: sanitizeHtml.defaults.allowedTags.concat(['img'])
+  })
+}
 
 const writeItunesTag = (tag, value = '', attrs = {}) => {
   const attrsStr = Object.entries(attrs).map(([key, value]) => `${key}="${value}"`).join(' ').trim()
@@ -23,7 +36,8 @@ export async function GET (context) {
 
   for (const post of epPosts) {
     const compiledPostContent = await post.compiledContent()
-    const xmlPostContent = `<![CDATA[ ${compiledPostContent} ]]>`
+    const sanitizedPostContent = sanitizePostHTML(compiledPostContent)
+    const xmlPostContent = `<![CDATA[ ${sanitizedPostContent} ]]>`
 
     items.push({
       title: post.frontmatter.title,
@@ -36,19 +50,26 @@ export async function GET (context) {
         type: post.frontmatter.filetype,
         length: post.frontmatter.filesize,
       },
-      description: compiledPostContent,
+      description: xmlPostContent,
       content: xmlPostContent,
       customData: [
+        // custom data for the episode.
         objIntoItunesTag({
           episodeType: 'full',
           author: SITE_AUTHOR,
+          title: post.frontmatter.title,
           subtitle: post.frontmatter.subtitle || '',
           duration: post.frontmatter.duration,
           explicit: 'no',
-          summary: xmlPostContent,
           keywords: post.frontmatter.tags.join(', ')
         }),
-        writeItunesTag('image', '', { href: joinWithBaseUrl('/images/episode-covers/', post.frontmatter.coverImage) })
+        // NOTE: <itunes:summary> tag is not necessary to add. it's used nowhere for the presentation according to https://help.apple.com/itc/podcasts_connect/#/itcb54353390
+        // writeItunesTag('summary', sanitizedPostContent),
+        writeItunesTag('image', '', {
+          href: post.frontmatter.coverImage
+            ? joinWithBaseUrl('/images/episode-covers/', post.frontmatter.coverImage)
+            : joinWithBaseUrl('/images/show-cover.jpg')
+        })
       ].join('\n')
     })
   }
@@ -58,9 +79,12 @@ export async function GET (context) {
     description: SITE_DESCRIPTION_COMMON,
     site: context.site,
     xmlns: {
-      itunes: 'http://www.itunes.com/dtds/podcast-1.0.dtd'
+      itunes: 'http://www.itunes.com/dtds/podcast-1.0.dtd',
+      content: 'http://purl.org/rss/1.0/modules/content/',
+      podcast: 'https://podcastindex.org/namespace/1.0'
     },
     customData: [
+      // custom data for the show.
       '<language>en-us</language>',
       `<copyright>${SITE_AUTHOR}</copyright>`,
       objIntoItunesTag({
@@ -74,9 +98,9 @@ export async function GET (context) {
           email: 'hi@okturtles.org'
         })
       }),
-      writeItunesTag('image', '', { href: joinWithBaseUrl('/images/turtle.png') }),
+      writeItunesTag('image', '', { href: joinWithBaseUrl('/images/show-cover.jpg') }),
       writeItunesTag('category', '', { text: 'Technology' }),
-      writeItunesTag('category', '', { text: 'Society & Culture' }),
+      writeItunesTag('category', '', { text: 'Society &amp; Culture' }),
       writeItunesTag('category', '', { text: 'Education' })
     ].join('\n'),
     items
