08f356dadee1cb419be5e204580d27fa64b0d87a
SebinSong <songxevin@gmail.com>
2025-08-27 11:07:18 +1200

    some customization prop for EpisodeList.vue

--- src/components/episode-list/EpisodeList.vue
+++ src/components/episode-list/EpisodeList.vue
@@ -1,22 +1,59 @@
 <template>
-  <ul class="c-episode-list">
-    <EpisodeCard v-for="episode in episodes"
-      tag="li"
-      :key="episode.frontmatter.permalink"
-      :episode-details="episode.details" />
-  </ul>
+  <div class="c-ep-list-section-wrapper">
+    <div v-if="showToolbar" class="c-table-toolbar">
+      <h2 v-if="listTitle" class="c-ep-section-title">{{ listTitle }}</h2>
+    </div>
+
+    <ul class="c-episode-list">
+      <EpisodeCard v-for="episode in episodes"
+        tag="li"
+        :key="episode.frontmatter.permalink"
+        :episode-details="episode.details" />
+    </ul>
+  </div>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue'
+import { ref, computed } from 'vue'
 import { getAllEpisodes } from '@/helpers'
 import EpisodeCard from './EpisodeCard.vue'
 
-const episodes = ref(await getAllEpisodes())
+interface ComponentProps {
+  listTitle?: string,
+  epList?: Array<any>
+}
+
+const props = defineProps<ComponentProps>()
+
+const showToolbar = computed<boolean>(() => {
+  return Boolean(props.listTitle)
+})
+
+const episodes = ref(Array.isArray(props.epList) ? props.epList : await getAllEpisodes())
 </script>
 
 <style scope lang="scss">
 @use '@/styles/variables' as *;
+
+.c-ep-list-section-wrapper {
+  position: relative;
+  width: 100%;
+  display: block;
+}
+
+.c-table-toolbar {
+  position: relative;
+  margin-bottom: 2rem;
+}
+
+.c-ep-section-title {
+  font-size: $font-lg;
+  font-weight: 600;
+
+	@include from($tablet) {
+		font-size: $font-heading-5;
+	}
+}
 
 .c-episode-list {
   position: relative;
